eb74ed7a35503b0e2a108ca5a5b68f7083cb9400
Thomas Gunn <110038671+TGOpen@users.noreply.github.com>
2023-11-06 22:48:44 -0600

    Update build.sh

--- .render/build.sh
+++ .render/build.sh
@@ -50,6 +50,6 @@ echo "################################################"
 python manage.py collectstatic --no-input
 if [[ $CREATE_SUPERUSER ]];
 then
-  python world_champ_2022/manage.py createsuperuser --no-input
+  python manage.py createsuperuser --no-input
 fi
-python manage.py createsuperuser --no-input
+
